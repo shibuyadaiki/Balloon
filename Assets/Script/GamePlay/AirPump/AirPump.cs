22113f19a9c1e9b0e58f5abf1e9d6d5b9856cb48
shibuyadaiki <shibuyadaiki684@gmail.com>
2017-09-15 23:44:51 +0900

    ポンプを表示した

---
+++ Assets/Script/GamePlay/AirPump/AirPump.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+
+public class AirPump : MonoBehaviour, IDragHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        var position = _pumpImage.position;
+        position.y = eventData.position.y;
+
+        _pumpImage.position = position;
+    }
+
+    private void Start()
+    {
+        Assert.IsNotNull(_pumpImage);
+    }
+
+
+    private void Update()
+    {
+
+    }
+
+    [SerializeField]
+    private Transform _pumpImage = null;
+}
